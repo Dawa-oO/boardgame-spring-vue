10811c3c5650a4d3c011a113d52ade38000c8c29
Gautier Lemasson <gautier.lemasson@intech.lu>
2020-10-05 14:08:57 +0200

    Change back URL

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 
 Vue.config.productionTip = false
 /* $backUrl = livebox admin > Informations system > Internet > Adresse IPv4 WAN */
-Vue.prototype.$backUrl = "http://86.216.117.105:1991"
+Vue.prototype.$backUrl = "http://86.216.32.227:1991"
 
 new Vue({
   router,
